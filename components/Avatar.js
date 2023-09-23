ac6925efb09a6fab4bb30b4070b375e372dfec98
Maria Virk <121379426+RiaVirk@users.noreply.github.com>
2023-09-23 22:42:59 +0200

    Portfolio Finish 1

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 const Avatar = () => {
     return (
-        <div className="hidden xl:flex xl:max-w-none" >
-            <Image src={'/astronaut1.png'} width={640} height={640} alt="" className="translate-z-0 w-full h-full" />
+        <div>
+            Avatar
         </div>
     );
 }
