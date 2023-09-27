2cf4933d8f496c85660f1f0a51938b8dcc3791f5
Maria Virk <121379426+RiaVirk@users.noreply.github.com>
2023-09-27 21:35:30 +0200

    Logo Updated

--- components/Circles.js
+++ components/Circles.js
@@ -1,9 +1,17 @@
+import Image from "next/image";
+
 const Circles = () => {
-    return (
-        <div>
-            Circles
-        </div>
-    );
-}
+  return (
+    <div className="w-[200px] xl:w-[300px] absolute -right-2 -bottom-2 mix-blend-color-dodge animate-pulse duration-75 z-10">
+      <Image
+        src={"/circles1.png"}
+        width={480}
+        height={403}
+        className="w-full h-full"
+        alt=""
+      />
+    </div>
+  );
+};
 
 export default Circles;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   content: [
     './app/**/*.{js,ts,jsx,tsx,mdx}',
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './Components/**/*.{js,ts,jsx,tsx,mdx}',
 
     // Or if using `src` directory:
     './src/**/*.{js,ts,jsx,tsx,mdx}',
@@ -24,13 +24,16 @@ module.exports = {
       colors: {
         primary: '#131424',
         secondary: '#393A47',
-        accent: '#F13024',
+        accent: '#00cccc',
+        space: '#00cccc',
+        xyz: '#00cccc',
       },
       backgroundImage: {
         explosion: 'url("/bg-explosion.png")',
         circles: 'url("/bg-circles.png")',
         circleStar: 'url("/circle-star.svg")',
         site: 'url("/site-bg.svg")',
+        space: 'url("/space.png")'
       },
       animation: {
         'spin-slow': 'spin 6s linear infinite',
