3e23031a5daef09ecf7da07fb53ec7b1a34667bb
RiaVirk <virkmariaofficial@gmail.com>
2026-03-05 00:17:40 +0100

    05-03-26_01

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -4,20 +4,24 @@ const workSlides = {
     {
       images: [
         {
-          title: "title",
-          path: "/thumb1.jpg",
+          title: "AI Event Concierge",
+          path: "/AI_Event_Concierge.png",
+          link: "https://ai-event-planner-lac.vercel.app/",
+        },
+        {
+          title: "Asylkries Eppstein e.V.",
+          path: "/Asylkries_Website.png",
+          link: "https://asylkreis-eppstein.de/",
+        },
+        {
+          title: "Liaison Co.",
+          path: "/Project1.png",
+          link: "https://liaisonco.com/",
         },
         {
           title: "title",
-          path: "/thumb2.jpg",
-        },
-        {
-          title: "title",
-          path: "/thumb3.jpg",
-        },
-        {
-          title: "title",
-          path: "/thumb4.jpg",
+          path: "/Project2.png",
+          link: "https://mv-dinner-basic.netlify.app/",
         },
       ],
     },
@@ -25,19 +29,19 @@ const workSlides = {
       images: [
         {
           title: "title",
-          path: "/thumb4.jpg",
+          path: "/New_Project_soon.png",
         },
         {
           title: "title",
-          path: "/thumb1.jpg",
+          path: "/New_Project_soon.png",
         },
         {
           title: "title",
-          path: "/thumb2.jpg",
+          path: "/New_Project_soon.png",
         },
         {
           title: "title",
-          path: "/thumb3.jpg",
+          path: "/New_Project_soon.png",
         },
       ],
     },
@@ -70,17 +74,30 @@ const WorkSlider = () => {
             <div className="grid grid-cols-2 grid-rows-2 gap-4 cursor-pointer">
               {slide.images.map((image, index) => {
                 return (
-                  <div className="relative rounded-lg overflow-hidden flex items-center justify-center group" key={index}>
-                    <div className="flex items-center justify-center relative overflow-hidden group" key={index}>
-                      <Image src={image.path} width={500} height={300} alt="" />
+                  <div
+                    className="relative rounded-lg overflow-hidden flex items-center justify-center group"
+                    key={index}
+                  >
+                    <div
+                      className="flex items-center justify-center relative overflow-hidden group"
+                      key={index}
+                    >
+                      <Image src={image.path} width={600} height={300} alt="" />
 
                       <div className="absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-80 transition-all duration-700"></div>
 
                       <div className="absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20 transition-all duration-300">
                         <div className="flex items-center gap-x-2 text-[13px] tracking-[0.2em]">
                           <div className="delay-150">LIVE</div>
-                          <div className="translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150">PROJECT</div>
-                          <div className="text-xl translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-300" ><BsArrowRight /></div>
+                          <div className="translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150">
+                            PROJECT
+                          </div>
+                          <a
+                            href={image.link}
+                            className="text-xl translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-300"
+                          >
+                            <BsArrowRight />
+                          </a>
                         </div>
                       </div>
                     </div>
--- pages/about/index.js
+++ pages/about/index.js
@@ -1,23 +1,27 @@
 import React, { useState } from "react";
 import ParticlesContainer from "../../Components/ParticlesContainer";
 
-
-import {
-  FaHtml5,
-  FaCss3,
-  FaJs,
-  FaReact,
-  FaWordpress,
-  FaFigma,
-} from "react-icons/fa";
+import { FaNodeJs, FaPython, FaGithub, FaFigma } from "react-icons/fa";
 
 import {
   SiNextdotjs,
   SiFramer,
-  SiAdobexd,
-  SiAdobephotoshop,
+  SiTypescript,
+  SiTailwindcss,
+  SiDjango,
+  SiPostgresql,
+  SiPytorch,
+  SiGoogleads,
+  SiGoogleanalytics,
+  SiCanva,
+  SiMeta,
+  SiShadcnui,
+  SiGooglegemini,
+  SiVercel,
+  SiOpenai,
+  SiFigma,
+  SiGooglemaps,
 } from "react-icons/si";
-
 
 const aboutData = [
   {
@@ -26,35 +30,38 @@ const aboutData = [
       {
         title: "Web Development",
         icons: [
-          { id: 1, title: <FaHtml5 /> },
-          { id: 2, title: <FaCss3 /> },
-          { id: 3, title: <FaJs /> },
-          { id: 4, title: <FaReact /> },
-          { id: 5, title: <SiNextdotjs /> },
-          { id: 6, title: <SiFramer /> },
-          { id: 7, title: <FaWordpress /> },
+          { id: 1, title: <SiNextdotjs /> },
+          { id: 2, title: <SiTypescript /> },
+          { id: 3, title: <FaNodeJs /> },
+          { id: 4, title: <SiTailwindcss /> },
+          { id: 5, title: <SiFramer /> },
+          { id: 6, title: <SiShadcnui /> },
+          { id: 7, title: <FaPython /> },
+          { id: 8, title: <SiDjango /> },
+          { id: 9, title: <SiPostgresql /> },
+          { id: 10, title: <FaGithub /> },
         ],
       },
       {
-        title: "UI/UX Design",
+        title: "AI & APIs",
         icons: [
-          { id: 1, title: <FaFigma /> },
-          { id: 2, title: <SiAdobexd /> },
-          { id: 3, title: <SiAdobephotoshop /> },
+          { id: 1, title: <SiOpenai /> },
+          { id: 2, title: <SiGooglegemini /> },
+          { id: 3, title: <SiPytorch /> },
+          { id: 4, title: <SiVercel /> },
+          { id: 5, title: <SiPostgresql /> },
+          { id: 6, title: <SiGooglemaps /> },
         ],
       },
-    ],
-  },
-  {
-    title: "awards",
-    info: [
       {
-        title: "Webby Awards - Honoree",
-        stage: "2011 - 2012",
-      },
-      {
-        title: "Adobe Design Achievement Awards - Finalist",
-        stage: "2009 - 2010",
+        title: "Digital Marketing",
+        icons: [
+          { id: 1, title: <SiGoogleads /> },
+          { id: 2, title: <SiGoogleanalytics /> },
+          { id: 3, title: <SiCanva /> },
+          { id: 4, title: <SiFigma /> },
+          { id: 5, title: <SiMeta /> },
+        ],
       },
     ],
   },
@@ -62,34 +69,35 @@ const aboutData = [
     title: "experience",
     info: [
       {
-        title: "UX/UI Designer - XYZ Company",
-        stage: "2012 - 2023",
+        title: "Digital Manager & Full-Stack Developer - Percuma by CKE",
+        stage: "2025 - ",
       },
       {
-        title: "Web Developer - ABC Agency",
-        stage: "2010 - 2012",
+        title: "Frontend Web developer - Liaison Co.",
+        stage: "2020 - 2025",
       },
       {
-        title: "Intern - DEF Corporation",
-        stage: "2008 - 2010",
+        title: "Digital Marketing Manager - Schützenhof Deli",
+        stage: "2017 - 2020",
+      },
+      {
+        title: "Interior Designer - Salman Shahzaib Ent. (Real Estate)",
+        stage: "2011 - 2016",
       },
     ],
   },
   {
     title: "credentials",
     info: [
-      {
-        title: "Web Development - ABC University, LA, CA",
-        stage: "2011",
-      },
-      {
-        title: "Computer Science Diploma - AV Technical Institute",
-        stage: "2009",
-      },
-      {
-        title: "Certified Graphic Designer - ABC Institute, Los Angeles, CA",
-        stage: "2006",
-      },
+      { title: "CS50AI - Harvard University", stage: "2025" },
+      { title: "CS50SQL - Harvard University", stage: "2025" },
+      { title: "CS50T - Harvard University", stage: "2024" },
+      { title: "CS50W - Harvard University", stage: "2023" },
+      { title: "CS50P - Harvard University", stage: "2023" },
+      { title: "CS50X - Harvard University", stage: "2023" },
+      { title: "Google Ads App Marketing Certification", stage: "2026" },
+      { title: "Canva Visual Suite - Canva", stage: "2026" },
+      { title: "Responsive Web Design - freeCodeCamp", stage: "2023" },
     ],
   },
 ];
@@ -104,10 +112,8 @@ import CountUp from "react-countup";
 
 const About = () => {
   const [index, setIndex] = useState(0);
-  console.log(index);
   return (
-    <div className="h-full bg-primary/30 py-32 text-center xl:text-left">
-      
+    <div className="h-full bg-primary/30 py-48 text-center xl:text-left overflow-x-auto">
       <Circles />
 
       <motion.div
@@ -121,10 +127,8 @@ const About = () => {
       </motion.div>
 
       <div className="container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6">
-        {/* Main Text Plus Intro */}
         <div className="flex-1 flex flex-col justify-center">
           <ParticlesContainer />
-
           <motion.h2
             variants={fadeIn("right", 0.6)}
             initial="hidden"
@@ -140,14 +144,12 @@ const About = () => {
             initial="hidden"
             animate="show"
             exit="hidden"
-            className="mx-w-[500px] mx-auto xl:mx-0 mb-6 xl:mb-12 px-2 xl:px-0 "
+            className="mx-w-[500px] mx-auto xl:mx-0 mb-6 xl:mb-12 px-2 xl:px-0"
           >
-            4 years ago, I began my journey as a developer. Since then, I
-            &#39;ve created in my team 14 web products. Optimized numerous
-            applications for max-speed and scalability. Mentored my junior team
-            members. Ensured the technical feasibility of 150+ UI/UX designs.
+            6 years ago, I began my journey as a developer. Since then, I
+            &#39;ve created 30+ web products, optimized applications for
+            max-speed and scalability, and built AI-powered solutions.
           </motion.p>
-          {/* experience counter */}
           <motion.div
             variants={fadeIn("right", 1.0)}
             initial="hidden"
@@ -158,44 +160,24 @@ const About = () => {
             <div className="flex flex-1 xl:gap-x-6">
               <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0">
                 <div className="text-2xl xl:text-4xl font-extrabold text-space mb-2">
-                  <CountUp start={0} end={4} duration={10} /> +
+                  <CountUp start={0} end={6} duration={10} /> +
                 </div>
                 <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">
                   Years of experience
                 </div>
               </div>
-              {/* 2nd Counter */}
               <div className="relative flex-1">
                 <div className="text-2xl xl:text-4xl font-extrabold text-space mb-2">
-                  <CountUp start={0} end={14} duration={10} /> +
+                  <CountUp start={0} end={30} duration={10} /> +
                 </div>
                 <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">
                   Web Products
-                </div>
-              </div>
-              {/* Unused Counter No.1 */}
-              <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0 hidden">
-                <div className="text-2xl xl:text-4xl font-extrabold text-space mb-2">
-                  <CountUp start={0} end={250} duration={5} /> +
-                </div>
-                <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">
-                  Not Used 1
-                </div>
-              </div>
-              {/* Unused Counter No.2 */}
-              <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0 hidden">
-                <div className="text-2xl xl:text-4xl font-extrabold text-space mb-2">
-                  <CountUp start={0} end={500} duration={5} /> +
-                </div>
-                <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">
-                  Not Used 2
                 </div>
               </div>
             </div>
           </motion.div>
         </div>
 
-        {/* Information */}
         <motion.div
           variants={fadeIn("left", 0.8)}
           initial="hidden"
@@ -204,43 +186,37 @@ const About = () => {
           className="flex flex-col w-full xl:max-w-[48%] h-[480px]"
         >
           <div className="flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4">
-            {aboutData.map((item, itemIndex) => {
-              return (
-                <div
-                  key={itemIndex}
-                  className={`${
-                    index === itemIndex &&
-                    "text-accent after:w-[100%] after:bg-xyz after:transition-all after:duration-300 "
-                  } cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute after:-bottom-1 after:left-0`}
-                  onClick={() => setIndex(itemIndex)}
-                >
-                  {item.title}
-                </div>
-              );
-            })}
+            {aboutData.map((item, itemIndex) => (
+              <div
+                key={itemIndex}
+                className={`${
+                  index === itemIndex &&
+                  "text-accent after:w-[100%] after:bg-xyz after:transition-all after:duration-300"
+                } cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute after:-bottom-1 after:left-0`}
+                onClick={() => setIndex(itemIndex)}
+              >
+                {item.title}
+              </div>
+            ))}
           </div>
-          <div className="py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4 items-center xl:items-start">
-            {aboutData[index].info.map((item, itemIndex) => {
-              return (
-                <div
-                  key={itemIndex}
-                  className="flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-white/60"
-                >
-                  <div className="font-light mb-2 md:mb-0">{item.title}</div>
-                  <div className="hidden md:flex">--</div>
-                  <div>{item.stage}</div>
-                  <div className="flex gap-x-4">
-                    {item.icons?.map((icon, itemIndex) => {
-                      return (
-                        <div className="text-2xl text-white" key={itemIndex}>
-                          {icon.title}
-                        </div>
-                      );
-                    })}
-                  </div>
+          <div className="py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4 items-center xl:items-start pb-32">
+            {aboutData[index].info.map((item, itemIndex) => (
+              <div
+                key={itemIndex}
+                className="flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-white/60"
+              >
+                <div className="font-light mb-2 md:mb-0">{item.title}</div>
+                <div className="hidden md:flex">-</div>
+                <div className="text-space">{item.stage}</div>
+                <div className="flex gap-x-4">
+                  {item.icons?.map((icon, iconIndex) => (
+                    <div className="text-2xl text-white" key={iconIndex}>
+                      {icon.title}
+                    </div>
+                  ))}
                 </div>
-              );
-            })}
+              </div>
+            ))}
           </div>
         </motion.div>
       </div>
